5666b76d521e3c836680314f37702a6273e2e9d6
p3ji <push.peji@gmail.com>
2026-07-07 14:30:21 -0400

    fix: add EC collective agreement to UI list

--- public/app.js
+++ public/app.js
@@ -146,6 +146,7 @@ function populateSourceDocuments() {
     { name: "PSC Retention/Layoff Guide", url: "https://www.canada.ca/en/public-service-commission/services/public-service-hiring-guides/selection-employees-retention-layoff-guide-managers-hr.html", type: "Guide" },
     { name: "CAPE Member Guide 2025", url: "https://www.acep-cape.ca/sites/default/files/2025-12/WFA2025MemberGuideEN20250530.pdf", type: "Union" },
     { name: "PSAC Member Guide 2025", url: "https://psacunion.ca/sites/psac/files/2025-psac-wfa-members-guide.pdf", type: "Union" },
+    { name: "EC Collective Agreement", url: "https://www.canada.ca/en/treasury-board-secretariat/topics/pay/collective-agreements/ec.html", type: "Union" },
     { name: "Directive on Leave", url: "https://www.tbs-sct.canada.ca/pol/doc-eng.aspx?id=15774", type: "Policy" }
   ];
 
